ce214d8b3cb2eac4583dfaf53cc01a6fd5ae9592
Costi <leopard_x3@yahoo.com>
2014-07-13 14:54:41 +0300

    bat file variable change

--- dobackup.bat
+++ dobackup.bat
@@ -1,3 +1,3 @@
 @SET PYTHON_HOME=d:\mind\programs\Python34\
-@SET SCRIPT_HOME=d:\mind\programs\python_scripts\
+@SET SCRIPT_HOME=d:\mind\programs\python_scripts\Backup
 %PYTHON_HOME%\python.exe %SCRIPT_HOME%\Backup.py %*
