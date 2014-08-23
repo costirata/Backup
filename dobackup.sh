93551885170496d122a6a3c6c8e454a54757ba46
Costi <costi.rata@yahoo.com>
2014-08-23 20:09:38 +0100

    Work version
    This is the version from work: 1.02.00

---
+++ dobackup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#__version__ = '1.02.00'
+PYTHON_HOME=/home/mind/mindbin/python3.4.1/
+SCRIPT_HOME=/home/mind/mindbin/python_scripts/backup
+$PYTHON_HOME/bin/python $SCRIPT_HOME/Backup.py $@
